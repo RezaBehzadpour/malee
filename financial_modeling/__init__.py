dd175a29713fefaea0197bb5966d915b36f59b46
Reza Behzadpour <reza.behzadpour1993@gmail.com>
2021-05-26 20:51:34 +0430

    feat(docstring): Explicitly using `google` format

--- financial_modeling/__init__.py
+++ financial_modeling/__init__.py
@@ -3,6 +3,7 @@ from ._financial_modeling import *
 
 __version__ = "0.0.2"
 __all__ = ["ar", "lr", "aar", "twr", "an", "pv", "npv", "gm"]
+__docformat__ = "google"
 
 # module level doc-string
 __doc__ = """
