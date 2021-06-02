b04f1c165dcc86443cf7dc6cdeed011e10c33f38
Reza Behzadpour <reza.behzadpour1993@gmail.com>
2021-06-03 01:51:48 +0430

    fix(typo): Fixing typos

--- malee/__init__.py
+++ malee/__init__.py
@@ -1,27 +1,27 @@
 from ._malee import *
 
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __all__ = ["ar", "lr", "aar", "twr", "an", "pv", "npv", "gm"]
 __docformat__ = "google"
 
 # module level doc-string
 __doc__ = """
-mali - a simple financial modeling library for Python
+malee - a simple financial modeling library for Python
 =====================================================================
-**mali** is a Python package providing simple, easy to undestand, and useful
+**malee** is a Python package providing simple, easy to undestand, and useful
 library designed to make people lives a little easier when dealing with financial
 modeling using Python. At the moment it aims to be easy enough so people feel home when 
 using it. Additionally, it has the broader goal of becoming **the goto library when people
 consider doing financial modeling in Python**. It is already well on its way toward this 
 goal.
 
-**mali** in Farsi means Financial. Tried so many other cool names, but I was late in the game
+**malee** in Farsi means Financial. Tried so many other cool names, but I was late in the game
 and they were already taken.
 
 Main Features
 -------------
-Here are just a few of the things that ``mali`` has at the moment:
+Here are just a few of the things that ``malee`` has at the moment:
   - A list of necessary rate of returns and risk functions ready to be used 
   - Easy to undestand code base so anyone can contribute to.
   - Enough documentation to get you started.
