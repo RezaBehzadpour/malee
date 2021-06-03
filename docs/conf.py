a19f0b0a670de1215af3a86ce7e808f0ed9735ca
Reza Behzadpour <reza.behzadpour1993@gmail.com>
2021-06-04 01:22:52 +0430

    feat(RTD): Adding more RTD files

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Reza Behzadpour"
 author = "Reza Behzadpour"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.2"
+release = "0.0.4"
 
 
 # -- General configuration ---------------------------------------------------
