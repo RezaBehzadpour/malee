cbd8683f40cc311df43d053a74c762418a976117
Reza Behzadpour <29193529+RezaBehzadpour@users.noreply.github.com>
2021-06-03 13:05:58 +0430

    rem(line): Removed unnecessary line

--- docs/conf.py
+++ docs/conf.py
@@ -58,5 +58,3 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
-
-autodoc_mock_imports = ["malee"]
