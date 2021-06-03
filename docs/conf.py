1608366d42c7b4c2b8ed6d7b314b515b4c1e14ce
Reza Behzadpour <reza.behzadpour1993@gmail.com>
2021-06-03 12:51:32 +0430

    doc(RTD): Change configs for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../malee/"))
+sys.path.insert(0, os.path.abspath("../malee"))
 
 
 # -- Project information -----------------------------------------------------
@@ -58,3 +58,5 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+autodoc_mock_imports = ["malee"]
